56cecb29062d50ecb3a1e35faf975daeb603bca3
anubhavcu <anubhav342y@gmail.com>
2020-12-11 01:15:15 +0530

    added background image in home page - background image styling in Home component minor changes in index.css

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,11 +1,19 @@
 import React from "react";
+import image from "../home_background.jpg";
 
-const Home = () => {
+export default function Home() {
   return (
-    <div>
-      <h1>Home</h1>
-    </div>
+    <main>
+      <img
+        src={image}
+        alt="computer keyboard"
+        className="absolute object-cover w-full h-full"
+      />
+      <section className="relative flex justify-center min-h-screen pt-12 lg:pt-64 px-8">
+        <h1 className="text-6xl text-green-100 font-bold cursive leading-none lg:leading-snug home-name">
+          Hello!. I'm Anubhav.
+        </h1>
+      </section>
+    </main>
   );
-};
-
-export default Home;
+}
