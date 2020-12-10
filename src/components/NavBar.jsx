54ed48b68c258d90bb97637111f3b2a351897252
anubhavcu <anubhav342y@gmail.com>
2020-12-10 22:46:21 +0530

    1. added react router to the project - added navbar navlinks with react-router in NavBar component
    2. changed google font
    3. added .eslintrc to gitignore- check later if creates a problem

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -3,15 +3,38 @@ import { NavLink } from "react-router-dom";
 
 export default function NavBar() {
   return (
-    <header>
-      <div>
-        <nav>
-          <NavLink to="/" exact>
+    <header className="bg-red-600">
+      <div className="container mx-auto flex justify-between">
+        <nav className="flex">
+          <NavLink
+            to="/"
+            exact
+            activeClassName="text-white"
+            className="inflex-flex items-center py-6 px-3 mr-10  text-red-100 hover:text-green-800 text-4xl sans-serif tracking-widest"
+          >
             Anubhav
           </NavLink>
-          <NavLink to="/post">Blog Posts</NavLink>
-          <NavLink to="/project">Projects</NavLink>
-          <NavLink to="/about">About Me!</NavLink>
+          <NavLink
+            to="/post"
+            activeClassName="text-red-100 bg-red-700"
+            className="inflex-flex items-center py-3 px-4 my-6 rounded text-red-200 hover:text-green-800"
+          >
+            Blog Posts
+          </NavLink>
+          <NavLink
+            to="/project"
+            activeClassName="text-red-100 bg-red-700"
+            className="inflex-flex items-center py-3 px-4 my-6 rounded text-red-200 hover:text-green-800"
+          >
+            Projects
+          </NavLink>
+          <NavLink
+            to="/about"
+            activeClassName="text-red-100 bg-red-700"
+            className="inflex-flex items-center py-3 px-4 my-6 rounded text-red-200 hover:text-green-800"
+          >
+            About Me!
+          </NavLink>
         </nav>
       </div>
     </header>
