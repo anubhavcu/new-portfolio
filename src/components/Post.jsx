178eaade8f09cd006f20197fcccd134991c4bebc
anubhavcu <anubhav342y@gmail.com>
2020-12-12 04:41:16 +0530

    added loading effect to Post and Project component
    added a isLoading state to both Project and Post components and set its
    default value as true. When data is fetched from the Sanity.io's server
    the state's isLoading value is changed to false. Depending upon the
    value of isLoading, either projects/blogPost are shown or 'Loading...' is
    shown.

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -4,6 +4,7 @@ import sanityClient from "../client.js";
 
 export default function Post() {
   const [postData, setPost] = useState(null);
+  const [isLoading, setLoading] = useState(true);
 
   useEffect(() => {
     sanityClient
@@ -22,42 +23,56 @@ export default function Post() {
       )
       .then((data) => {
         console.log(data);
+        setLoading(false);
         setPost(data);
       })
       .catch(console.error);
   }, []);
-  return (
-    <main className="bg-green-00 min-h-screen p-12">
-      <section className="container mx-auto">
-        <h1 className="text-5xl flex justify-center cursive">Blog Post Page</h1>
-        <h2 className="text-lg text-gray-600 flex justify-center mb-12">
-          Welcome to my page of Blog Posts.
-        </h2>
-        <div className="grid md:grid-cols-2 lg:grid-cols-3 gap-8">
-          {postData &&
-            postData.map((post, index) => (
-              <article className=" transition duration-300 ease-in-out transform hover:-translate-y-1 hover:scale-70">
-                <Link to={"/post/" + post.slug.current} key={post.slug.current}>
-                  <span
-                    className="block h-64 relative rounded shadow leading-snug border-l-8 border-green-400"
-                    key={index}
+  if (!isLoading) {
+    return (
+      <main className="bg-green-00 min-h-screen p-12">
+        <section className="container mx-auto">
+          <h1 className="text-5xl flex justify-center cursive">
+            Blog Post Page
+          </h1>
+          <h2 className="text-lg text-gray-600 flex justify-center mb-12">
+            Welcome to my page of Blog Posts.
+          </h2>
+          <div className="grid md:grid-cols-2 lg:grid-cols-3 gap-8">
+            {postData &&
+              postData.map((post, index) => (
+                <article className=" transition duration-300 ease-in-out transform hover:-translate-y-1 hover:scale-70">
+                  <Link
+                    to={"/post/" + post.slug.current}
+                    key={post.slug.current}
                   >
-                    <img
-                      src={post.mainImage.asset.url}
-                      alt={post.mainImage.alt}
-                      className="w-full h-full rounded-r object-cover absolute"
-                    />
-                    <span className="block relative h-full flex justify-end items-end pr-4 pb-4">
-                      <h3 className="text-gray-800 text-lg font-blog px-3 py-4 bg-red-700 text-red-100 bg-opacity-75 rounded">
-                        {post.title}
-                      </h3>
+                    <span
+                      className="block h-64 relative rounded shadow leading-snug border-l-8 border-green-400"
+                      key={index}
+                    >
+                      <img
+                        src={post.mainImage.asset.url}
+                        alt={post.mainImage.alt}
+                        className="w-full h-full rounded-r object-cover absolute"
+                      />
+                      <span className="block relative h-full flex justify-end items-end pr-4 pb-4">
+                        <h3 className="text-gray-800 text-lg font-blog px-3 py-4 bg-red-700 text-red-100 bg-opacity-75 rounded">
+                          {post.title}
+                        </h3>
+                      </span>
                     </span>
-                  </span>
-                </Link>
-              </article>
-            ))}
-        </div>
+                  </Link>
+                </article>
+              ))}
+          </div>
+        </section>
+      </main>
+    );
+  } else {
+    return (
+      <section className="relative flex justify-center min-h-screen pt-12 lg:pt-64 px-8">
+        <h2 className="text-4xl">Loading ... </h2>;
       </section>
-    </main>
-  );
+    );
+  }
 }
--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -3,6 +3,7 @@ import sanityClient from "../client";
 
 export default function Project() {
   const [projectData, setProjectData] = useState(null);
+  const [isLoading, setLoading] = useState(true);
 
   useEffect(() => {
     sanityClient
@@ -19,84 +20,96 @@ export default function Project() {
        }`
       )
       .then((data) => {
-        console.log(data);
+        // console.log(data);
+        setLoading(false);
         setProjectData(data);
       })
       .catch(console.error);
   }, []);
-
-  return (
-    <main className="bg-green-100 min-h-screen p-12">
-      <section className="container mx-auto">
-        <h1 className="text-5xl flex justify-center cursive"> My Projects </h1>
-        <h2 className="text-lg text-gray-600 flex justify-center mb-12">
-          Welcome to my project page!
-        </h2>
-        <section className="grid grid-cols-2 gap-8">
-          {projectData &&
-            projectData.map((project, index) => (
-              //   <a
-              //     href={project.link}
-              //     alt={project.title}
-              //     target="_blank"
-              //     rel="noopener noreferrer"
-              //   >
-              <article className="relative rounded-lg shadow-xl bg-white p-16 transition duration-300 ease-in-out   transform hover:-translate-y-1 hover:scale-60 ">
-                <h3 className="text-gray-800 text-3xl font-bold mb-2 hover:text-red-700">
-                  <a
-                    href={project.link}
-                    alt={project.title}
-                    target="_blank"
-                    rel="noopener noreferrer"
-                  >
-                    {project.title}
-                  </a>
-                </h3>
-                <div className="text-gray-500 text-xs space-x-4">
-                  <span>
-                    <strong className="font-bold">Finished on</strong>:{" "}
-                    {new Date(project.date).toLocaleDateString()}
-                  </span>
-                  <span>
-                    <strong className="font-bold">Place</strong>:{" "}
-                    {project.place}
-                  </span>
-                  <span className="font-bold">
-                    <strong>Type</strong>: {project.projectType}
-                  </span>{" "}
-                  <p className="my-6 text-lg text-gray-700 leading-relaxed">
-                    {project.description}
-                  </p>
-                  <div className="mb-4">
+  //   const { isLoading } = this.state;
+  if (!isLoading) {
+    return (
+      <main className="bg-green-100 min-h-screen p-12">
+        <section className="container mx-auto">
+          <h1 className="text-5xl flex justify-center cursive">
+            {" "}
+            My Projects{" "}
+          </h1>
+          <h2 className="text-lg text-gray-600 flex justify-center mb-12">
+            Welcome to my project page!
+          </h2>
+          <section className="grid grid-cols-2 gap-8">
+            {projectData &&
+              projectData.map((project, index) => (
+                //   <a
+                //     href={project.link}
+                //     alt={project.title}
+                //     target="_blank"
+                //     rel="noopener noreferrer"
+                //   >
+                <article className="relative rounded-lg shadow-xl bg-white p-16 transition duration-300 ease-in-out   transform hover:-translate-y-1 hover:scale-60 ">
+                  <h3 className="text-gray-800 text-3xl font-bold mb-2 hover:text-red-700">
                     <a
                       href={project.link}
+                      alt={project.title}
+                      target="_blank"
+                      rel="noopener noreferrer"
+                    >
+                      {project.title}
+                    </a>
+                  </h3>
+                  <div className="text-gray-500 text-xs space-x-4">
+                    <span>
+                      <strong className="font-bold">Finished on</strong>:{" "}
+                      {new Date(project.date).toLocaleDateString()}
+                    </span>
+                    <span>
+                      <strong className="font-bold">Place</strong>:{" "}
+                      {project.place}
+                    </span>
+                    <span className="font-bold">
+                      <strong>Type</strong>: {project.projectType}
+                    </span>{" "}
+                    <p className="my-6 text-lg text-gray-700 leading-relaxed">
+                      {project.description}
+                    </p>
+                    <div className="mb-4">
+                      <a
+                        href={project.link}
+                        rel="noopener noreferrer"
+                        target="_blank"
+                        className="text-red-500 font-bold hover:underline hover:text-red-400 text-xl"
+                      >
+                        View the Project{" "}
+                        <span role="img" aria-label="right pointer">
+                          👉
+                        </span>
+                      </a>
+                    </div>
+                    <a
+                      href={project.code}
                       rel="noopener noreferrer"
                       target="_blank"
                       className="text-red-500 font-bold hover:underline hover:text-red-400 text-xl"
                     >
-                      View the Project{" "}
+                      View Source Code{" "}
                       <span role="img" aria-label="right pointer">
                         👉
                       </span>
                     </a>
                   </div>
-                  <a
-                    href={project.code}
-                    rel="noopener noreferrer"
-                    target="_blank"
-                    className="text-red-500 font-bold hover:underline hover:text-red-400 text-xl"
-                  >
-                    View Source Code{" "}
-                    <span role="img" aria-label="right pointer">
-                      👉
-                    </span>
-                  </a>
-                </div>
-              </article>
-              //   </a>
-            ))}
+                </article>
+                //   </a>
+              ))}
+          </section>
         </section>
+      </main>
+    );
+  } else {
+    return (
+      <section className="relative flex justify-center min-h-screen pt-12 lg:pt-64 px-8">
+        <h2 className="text-4xl">Loading ... </h2>;
       </section>
-    </main>
-  );
+    );
+  }
 }
