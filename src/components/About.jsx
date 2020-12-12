3955b81140d175ddb10f59fda5626be951bf8d81
anubhavcu <anubhav342y@gmail.com>
2020-12-12 16:41:40 +0530

    added content in About component
    this component will fetch data such as bio, profile image etc from sanity.io server and display results
    accordingly. Similar to SinglePost component, BlockContent and
    imageUrlBuilder has been used.
    Some styling such as loader, displaying profile as card etc has been
    done.
    Background image of About page is been loaded from project's src folder.

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,11 +1,72 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import sanityClient from "../client.js";
+import background from "../about_background.jpg";
+import image from "../spinner.gif";
+import BlockContent from "@sanity/block-content-to-react";
+import imageUrlBuilder from "@sanity/image-url";
 
-const About = () => {
+const builder = imageUrlBuilder(sanityClient);
+function urlFor(source) {
+  return builder.image(source);
+}
+
+export default function About() {
+  const [author, setAuthor] = useState(null);
+  useEffect(() => {
+    sanityClient
+      .fetch(
+        `*[_type == 'author']{
+            name, 
+            bio, 
+            "authorImage" : image.asset->url
+
+        }`
+      )
+      .then((data) => setAuthor(data[0]))
+      .catch(console.error);
+  }, []);
+  if (!author) {
+    return (
+      <div className="relative flex justify-center min-h-screen pt-12 lg:pt-64 px-8">
+        {/* <h2 className="text-4xl">Loading.. </h2> */}
+        <h2 className="text-4xl justify-center  ">
+          {" "}
+          <img src={image} className="justify-center"></img>{" "}
+        </h2>
+        ;
+      </div>
+    );
+  }
   return (
-    <div>
-      <h2>About</h2>
-    </div>
-  );
-};
+    <main className="relative ">
+      <img
+        src={background}
+        alt="Background image"
+        className="absolute w-full"
+      />
+      <div className="p-10 lg:pt-48 container mx-auto relative">
+        <section className="bg-green-800 rounded-lg shadow-2xl lg:flex p-20">
+          <img
+            src={urlFor(author.authorImage).url()}
+            alt={author.name}
+            className="rounded w-32 h-32 lg:w-64 lg:h-64 mr-8"
+          />
 
-export default About;
+          <div className="text-lg flex flex-col justify-center">
+            <h1 className="cursive text-6xl text-green-300 mb-4 ">
+              Hey There. I'm{" "}
+              <span className="text-green-50 ">{author.name}</span>
+            </h1>
+            <div className="prose lg:prose-xl text-white">
+              <BlockContent
+                blocks={author.bio}
+                projectId="ioe8ozk3"
+                dataset="production"
+              />
+            </div>
+          </div>
+        </section>
+      </div>
+    </main>
+  );
+}
