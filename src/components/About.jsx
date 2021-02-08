5f3050edfc1379a0afba3ab9cb354097ba0508b7
anubhavcu <anubhav342y@gmail.com>
2021-02-08 14:05:22 +0530

    minor changes in links and bio

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,9 +1,9 @@
-import React, { useEffect, useState } from "react";
-import sanityClient from "../client.js";
-import background from "../about_background.jpg";
-import image from "../spinner.gif";
-import BlockContent from "@sanity/block-content-to-react";
-import imageUrlBuilder from "@sanity/image-url";
+import React, { useEffect, useState } from 'react';
+import sanityClient from '../client.js';
+import background from '../about_background.jpg';
+import image from '../spinner.gif';
+import BlockContent from '@sanity/block-content-to-react';
+import imageUrlBuilder from '@sanity/image-url';
 
 const builder = imageUrlBuilder(sanityClient);
 function urlFor(source) {
@@ -27,55 +27,55 @@ export default function About() {
   }, []);
   if (!author) {
     return (
-      <div className="relative flex justify-center min-h-screen pt-12 lg:pt-64 px-8">
+      <div className='relative flex justify-center min-h-screen pt-12 lg:pt-64 px-8'>
         {/* <h2 className="text-4xl">Loading.. </h2> */}
-        <h2 className="text-4xl justify-center  ">
-          {" "}
-          <img src={image} className="justify-center"></img>{" "}
+        <h2 className='text-4xl justify-center  '>
+          {' '}
+          <img src={image} className='justify-center'></img>{' '}
         </h2>
         ;
       </div>
     );
   }
   return (
-    <main className="relative ">
+    <main className='relative '>
       <img
         src={background}
-        alt="Background image"
-        className="absolute w-full"
+        alt='Background image'
+        className='absolute w-full'
       />
-      <div className="p-10 lg:pt-48 container mx-auto relative">
-        <section className="bg-green-800 rounded-lg shadow-2xl lg:flex p-20">
+      <div className='p-10 lg:pt-48 container mx-auto relative'>
+        <section className='bg-green-800 rounded-lg shadow-2xl lg:flex p-20'>
           <img
             src={urlFor(author.authorImage).url()}
             alt={author.name}
-            className="rounded w-32 h-32 lg:w-64 lg:h-64 mr-8"
+            className='rounded w-32 h-32 lg:w-64 lg:h-64 mr-8'
           />
 
-          <div className="text-lg flex flex-col justify-center">
-            <h1 className="cursive text-6xl text-green-300 mb-4 ">
-              Hey There. I'm{" "}
-              <span className="text-green-50 ">{author.name}</span>
+          <div className='text-lg flex flex-col justify-center'>
+            <h1 className='cursive text-6xl text-green-300 mb-4 '>
+              Hey There. I'm{' '}
+              <span className='text-green-50 '>{author.name}</span>
             </h1>
-            <div className="prose lg:prose-xl text-white">
+            <div className='prose lg:prose-xl text-white'>
               <BlockContent
                 blocks={author.bio}
-                projectId="ioe8ozk3"
-                dataset="production"
+                projectId='ioe8ozk3'
+                dataset='production'
               />
-              <h4 style={{ color: "#fff" }}>
-                Check on my{" "}
+              <h4 style={{ color: '#fff' }}>
                 <strong>
                   <a
-                    href="https://drive.google.com/file/d/16l1KhoQAUMQ3Z0C9r1sHO0hHDLrGvQur/view?usp=sharing"
-                    target="_blank"
+                    // href='https://drive.google.com/file/d/16l1KhoQAUMQ3Z0C9r1sHO0hHDLrGvQur/view?usp=sharing'
+                    href='mailto:anubhavcs.py@gmail.com'
+                    target='_blank'
                   >
-                    <button class="py-1 px-3 bg-red-500 text-white font-semibold rounded-lg shadow-md hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-green-400 focus:ring-opacity-75 transition duration-300 ease-in-out   transform hover:-translate-y-1 hover:scale-60 ">
-                      Resume
+                    <button class='py-1 px-3 bg-red-500 text-white font-semibold rounded-lg shadow-md hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-green-400 focus:ring-opacity-75 transition duration-300 ease-in-out   transform hover:-translate-y-1 hover:scale-60 '>
+                      Connect
                     </button>
-                  </a>{" "}
-                </strong>{" "}
-                for more info!
+                  </a>{' '}
+                </strong>{' '}
+                with me for more info!
               </h4>
               {/* <Skills /> */}
             </div>
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,86 +1,87 @@
-import React from "react";
-import { NavLink } from "react-router-dom";
-import { SocialIcon } from "react-social-icons";
-import { BiDownload } from "react-icons/bi";
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+import { SocialIcon } from 'react-social-icons';
+import { BiDownload } from 'react-icons/bi';
 
 export default function NavBar() {
   return (
-    <header className="bg-red-600">
-      <div className="container mx-auto flex justify-between">
-        <nav className="flex">
+    <header className='bg-red-600'>
+      <div className='container mx-auto flex justify-between'>
+        <nav className='flex'>
           <NavLink
-            to="/"
+            to='/'
             exact
-            activeClassName="text-white"
-            className="inflex-flex items-center py-6 px-3 mr-10  text-red-100 transition duration-300 ease-in-out  hover:bg-red-600  hover:text-white text-4xl cursive tracking-widest"
+            activeClassName='text-white'
+            className='inflex-flex items-center py-6 px-3 mr-10  text-red-100 transition duration-300 ease-in-out  hover:bg-red-600  hover:text-white text-4xl cursive tracking-widest'
           >
             Anubhav
           </NavLink>
           <NavLink
-            to="/post"
-            activeClassName="text-red-100 bg-red-700"
-            className="inflex-flex items-center py-3 px-4 my-6 mx-1 rounded text-red-200 transition duration-300 ease-in-out  hover:bg-red-600 transform hover:-translate-y-1 hover:scale-110 hover:text-green-500"
+            to='/post'
+            activeClassName='text-red-100 bg-red-700'
+            className='inflex-flex items-center py-3 px-4 my-6 mx-1 rounded text-red-200 transition duration-300 ease-in-out  hover:bg-red-600 transform hover:-translate-y-1 hover:scale-110 hover:text-green-500'
           >
             Blog Posts
           </NavLink>
           <NavLink
-            to="/project"
-            activeClassName="text-red-100 bg-red-700"
-            className="inflex-flex items-center py-3 px-4 my-6 mx-1 rounded text-red-200 transition duration-300 ease-in-out  hover:bg-red-600 transform hover:-translate-y-1 hover:scale-110 hover:text-green-500 hover:text-red-100"
+            to='/project'
+            activeClassName='text-red-100 bg-red-700'
+            className='inflex-flex items-center py-3 px-4 my-6 mx-1 rounded text-red-200 transition duration-300 ease-in-out  hover:bg-red-600 transform hover:-translate-y-1 hover:scale-110 hover:text-green-500 hover:text-red-100'
           >
             Projects
           </NavLink>
           <NavLink
-            to="/about"
-            activeClassName="text-red-100 bg-red-700"
-            className="inflex-flex items-center py-3 px-4 my-6 mx-1 rounded text-red-200 transition duration-300 ease-in-out  hover:bg-red-600 transform hover:-translate-y-1 hover:scale-110 hover:text-green-500 hover:text-red-100"
+            to='/about'
+            activeClassName='text-red-100 bg-red-700'
+            className='inflex-flex items-center py-3 px-4 my-6 mx-1 rounded text-red-200 transition duration-300 ease-in-out  hover:bg-red-600 transform hover:-translate-y-1 hover:scale-110 hover:text-green-500 hover:text-red-100'
           >
             About Me!
           </NavLink>
         </nav>
-        <div className="inline-flex py-3 px-4 my-6">
+        <div className='inline-flex py-3 px-4 my-6'>
           <SocialIcon
-            url="https://github.com/anubhavcu"
-            className="mr-5 transition duration-300 ease-in-out  transform hover:-translate-y-1 hover:scale-150"
-            target="_blank"
-            fgColor="#fff"
+            url='https://github.com/anubhavcu'
+            className='mr-5 transition duration-300 ease-in-out  transform hover:-translate-y-1 hover:scale-150'
+            target='_blank'
+            fgColor='#fff'
             style={{ height: 35, width: 35 }}
           />
           <SocialIcon
-            url="https://www.linkedin.com/in/anubhavksr/"
-            className="mr-5 transition duration-300 ease-in-out   transform hover:-translate-y-1 hover:scale-150"
-            target="_blank"
-            fgColor="#fff"
+            url='https://www.linkedin.com/in/anubhavksr/'
+            className='mr-5 transition duration-300 ease-in-out   transform hover:-translate-y-1 hover:scale-150'
+            target='_blank'
+            fgColor='#fff'
             style={{ height: 35, width: 35 }}
           />
           <SocialIcon
-            url="mailto:anubhavcs.py@gmail.com"
-            className="mr-5 transition duration-300 ease-in-out  transform hover:-translate-y-1 hover:scale-150"
-            target="_blank"
-            fgColor="#fff"
+            url='mailto:anubhavcs.py@gmail.com'
+            className='mr-5 transition duration-300 ease-in-out  transform hover:-translate-y-1 hover:scale-150'
+            target='_blank'
+            fgColor='#fff'
             style={{ height: 35, width: 35 }}
           />
           <SocialIcon
-            url="https://twitter.com/anubhav_sr"
-            className="mr-5 transition duration-300 ease-in-out   transform hover:-translate-y-1 hover:scale-150"
-            target="_blank"
-            fgColor="#fff"
+            url='https://twitter.com/anubhav_sr'
+            className='mr-5 transition duration-300 ease-in-out   transform hover:-translate-y-1 hover:scale-150'
+            target='_blank'
+            fgColor='#fff'
             style={{ height: 35, width: 35 }}
           />
           <SocialIcon
             // url="https://wa.me/919315973298"
-            url="https://facebook.com"
-            className="mr-5 transition duration-300 ease-in-out  transform hover:-translate-y-1 hover:scale-150"
-            target="_blank"
-            fgColor="#fff"
+            url='https://facebook.com'
+            className='mr-5 transition duration-300 ease-in-out  transform hover:-translate-y-1 hover:scale-150'
+            target='_blank'
+            fgColor='#fff'
             style={{ height: 35, width: 35 }}
           />
-          {/* <a
-            href="https://drive.google.com/file/d/16l1KhoQAUMQ3Z0C9r1sHO0hHDLrGvQur/view?usp=sharing"
-            target="_blank"
-          > */}
-            <h3 className="mr-5 transition duration-300 ease-in-out  transform hover:-translate-y-1 hover:scale-150">
-              <BiDownload style={{ height: 35, width: 35, color: "#fff" }} />
+          <a
+            // href="https://drive.google.com/file/d/16l1KhoQAUMQ3Z0C9r1sHO0hHDLrGvQur/view?usp=sharing"
+            href='https://google.com'
+            target='_blank'
+          >
+            <h3 className='mr-5 transition duration-300 ease-in-out  transform hover:-translate-y-1 hover:scale-150'>
+              <BiDownload style={{ height: 35, width: 35, color: '#fff' }} />
             </h3>
           </a>
         </div>
